6b84e020eabc416b2811f34768e7b54f9be5017c
Alex <format37@gmail.com>
2023-11-15 01:24:39 +0400

    modified:   Dockerfile 	modified:   app.py

--- site/app.py
+++ site/app.py
@@ -7,4 +7,5 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', ssl_context=('cert.pem', 'key.pem'))
+    # app.run(debug=True, host='0.0.0.0', ssl_context=('cert.pem', 'key.pem'))
+    app.run(debug=True, host='0.0.0.0')
