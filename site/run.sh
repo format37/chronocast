b40a465e6c3908f339e7cef2089fd6e6a14f7dea
Alex <format37@gmail.com>
2023-11-15 01:27:31 +0400

    modified:   run.sh

--- site/run.sh
+++ site/run.sh
@@ -3,11 +3,5 @@ sudo docker rm site
 #   --restart always \  
 #   -d \
 sudo docker run \
-    --gpus '"device=0"' \
     --name site \
-    -v $(pwd)/cache:/app/cache \
-    -v /home/alex/projects/chronocast/data:/app/data \
-    -v $(pwd)/config.json:/app/config.json \
-    -v $(pwd)/credentials_full.json:/app/credentials_full.json \
-    -e GOOGLE_APPLICATION_CREDENTIALS=/app/credentials_full.json \
     -t site
