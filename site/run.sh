42f3187972e68f3da0e907c5e69f866374dcedab
Alex <format37@gmail.com>
2023-11-18 23:17:16 +0400

    modified:   site/Dockerfile 	deleted:    site/README.md 	deleted:    site/app.py 	deleted:    site/build.sh 	new file:   site/chronocast/__init__.py 	new file:   site/chronocast/asgi.py 	new file:   site/chronocast/settings.py 	new file:   site/chronocast/urls.py 	new file:   site/chronocast/wsgi.py 	new file:   site/db.sqlite3 	new file:   site/docker-compose.yml 	new file:   site/locale/en/LC_MESSAGES/django.mo 	new file:   site/locale/en/LC_MESSAGES/django.po 	new file:   site/locale/ru/LC_MESSAGES/django.mo 	new file:   site/locale/ru/LC_MESSAGES/django.po 	new file:   site/locale/uk/LC_MESSAGES/django.mo 	new file:   site/locale/uk/LC_MESSAGES/django.po 	deleted:    site/logs.sh 	new file:   site/main/__init__.py 	new file:   site/main/admin.py 	new file:   site/main/apps.py 	new file:   site/main/description_en.txt 	new file:   site/main/description_ru.txt 	new file:   site/main/description_uk.txt 	new file:   site/main/migrations/__init__.py 	new file:   site/main/migrations/__pycache__/__init__.cpython-312.pyc 	new file:   site/main/migrations/__pycache__/__init__.cpython-38.pyc 	new file:   site/main/migrations/__pycache__/__init__.cpython-39.pyc 	new file:   site/main/models.py 	new file:   site/main/static/CSS/style.css 	new file:   site/main/static/img/pat.png 	new file:   site/main/static/img/tel.png 	new file:   site/main/templates/main/index.html 	new file:   site/main/tests.py 	new file:   site/main/urls.py 	new file:   site/main/views.py 	new file:   site/manage.py 	modified:   site/requirements.txt 	modified:   site/run.sh 	deleted:    site/templates/index.html

--- site/run.sh
+++ site/run.sh
@@ -1,11 +1,2 @@
-sudo docker stop site
-sudo docker rm site
-#   --restart always \
-#   -d \
-# --network host \
-sudo docker run \
-    --name site \
-    --restart always \
-    -d \
-    -p 443:443 \
-    -t site
+# docker-compose up --build -d
+docker-compose up --build
