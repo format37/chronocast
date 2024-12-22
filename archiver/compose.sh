a72f41982521e94c10bcaedb02fb5cd493d016ca
Alex <format37@gmail.com>
2024-12-22 14:03:55 +0400

    modified:   compose.sh

--- archiver/compose.sh
+++ archiver/compose.sh
@@ -1,1 +1,8 @@
-sudo docker-compose up --build --d --force-recreate
+# sudo docker-compose up --build --d --force-recreate
+# Build
+docker build -t bq-export-service .
+
+# Run with mounted credentials
+docker run -v ./credentials_full.json:/app/credentials.json \
+  -e GOOGLE_APPLICATION_CREDENTIALS=/app/credentials.json \
+  bq-export-service
